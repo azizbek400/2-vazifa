1e7142b8e91e1bcd07b61a124f68642113f3a6fd
azizbek400 <aziznormurodov400@gmail.com>
2024-01-27 02:49:41 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,131 @@
+// 1-masala
+
+// let a = 15
+// console.log('Musbat');
+
+// 2-masala
+
+// let a = 19
+// console.log('Toq');
+
+// 3-masala
+
+// let a = 3
+// console.log(a > 2);
+// console.log(a >= 3);
+
+// 4-masala
+
+// let a = 5
+// let b = 5
+// let c = 5
+// console.log(a <= b < c);
+
+// 5-masala
+
+// let a = 6
+// let b = 11
+// console.log('a soni juft');
+// console.log('b soni toq');
+
+// 6-masala
+
+// let a = 20
+// let b = -8
+// let c = -16
+// console.log('a musbat');
+// console.log('b manfiy');
+// console.log('c manfiy');
+
+// 7-masala
+
+// let a = 317
+// console.log('hammasi har xil');
+
+// 8-masala
+
+// let a = 3
+// let b = 8
+// let c = 8
+// console.log(b >= c);
+// console.log('b bilan c bir xil son');
+
+// 9-masala
+
+// let a = 16
+// console.log('Juft');
+
+// 10-masala
+
+// let a = 42
+// let b = 60
+// console.log('a va b butun son');
+
+// 11-masala
+
+// let a = 3;
+// let b = 4;
+// let y = -b/2 * (a**2 + b**2);
+// console.log(y);
+
+
+// 12-masala
+
+// let a = 2;
+// let b = 3;
+// let c = 4;
+// let d = 5;
+// let y = (a + b/c + d)**2 + b**2
+// console.log(y);
+
+
+// 13-masala
+
+// let a = 2;
+// let d = 3;
+// let c = 6;
+// let v = a**d;
+// let f = (4 * c)**2/3
+// let y = 2 * (v + f)
+// console.log(y);
+
+
+// 14-masala
+
+// let a = 1;
+// let b = 20;
+// let c = 3;
+// let d = 7;
+// let f = 1/a**2;
+// let g = (b/10)**3;
+// let h = (c + d)**2;
+// let y = f * g * h;
+// console.log(y);
+
+
+// 15-masala
+
+// let a = 10;
+// let b = 50;
+// let c = 20;
+// let y = (a**2 + b**2 + c**2)/(a * b * c);
+// console.log(y);
+
+// 16-masala
+
+// let a = 363
+// console.log('bir xil');
+
+// 17-masala
+
+// let a = 773
+// console.log('3 xonali toq son');
+
+
+// 18-masala
+
+// let a = 234
+// console.log(ketma-ket);
+
+
+
